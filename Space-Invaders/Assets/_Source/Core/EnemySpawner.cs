cea9e1d5471237f3e5b876111f206f6e71110aa0
MeylishDLC <meylishy20@gmail.com>
2024-09-26 17:57:44 +0300

    feat: multiple enemy rows spawn

--- Space-Invaders/Assets/_Source/Core/EnemySpawner.cs
+++ Space-Invaders/Assets/_Source/Core/EnemySpawner.cs
@@ -1,25 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Core
 {
     public class EnemySpawner: MonoBehaviour
     {
-        [SerializeField] private GameObject enemyPrefab;
-        [SerializeField] private int numberOfEnemies = 10;
-        [SerializeField] private float spacing = 1.5f;
+        [SerializeField] private GameObject[] enemyPrefabs;
+        [SerializeField] private int numberOfEnemiesInRow = 10;
+        [SerializeField] private float spacing = 1f;
+        [SerializeField] private float rowSpacing = 1f;
+        [SerializeField] private float initialRowYPosition;
         [SerializeField] private Transform spawnParent;
-
+        
         private void Start()
         {
-            SpawnEnemiesInRow();
+            SpawnAllTypesOfEnemies();
         }
-        private void SpawnEnemiesInRow()
+
+        private void SpawnAllTypesOfEnemies()
         {
-            var totalWidth = (numberOfEnemies - 1) * spacing;
+            var currentY = initialRowYPosition;
 
-            var startPosition = new Vector3(-totalWidth / 2, 0, 0);
+            foreach (var enemyPrefab in enemyPrefabs)
+            {
+                SpawnEnemiesInRow(enemyPrefab, currentY);
+                currentY -= rowSpacing;
+            }
+        }
+        private void SpawnEnemiesInRow(GameObject enemyPrefab, float yOffset)
+        {
+            var totalWidth = (numberOfEnemiesInRow - 1) * spacing;
+            var startPosition = new Vector3(-totalWidth / 2, yOffset, 0);
 
-            for (var i = 0; i < numberOfEnemies; i++)
+            for (var i = 0; i < numberOfEnemiesInRow; i++)
             {
                 var spawnPosition = transform.position + startPosition + new Vector3(i * spacing, 0, 0);
                 var enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
