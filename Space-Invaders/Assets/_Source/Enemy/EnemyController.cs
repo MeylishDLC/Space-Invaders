896066c5aa15945c7f70bbf3befa701b7ce13da1
MeylishDLC <meylishy20@gmail.com>
2024-10-02 01:43:07 +0300

    feat: added animations

--- Space-Invaders/Assets/_Source/Enemy/EnemyController.cs
+++ Space-Invaders/Assets/_Source/Enemy/EnemyController.cs
@@ -12,17 +12,19 @@ namespace Enemy
     public class EnemyController: MonoBehaviour
     {
         public event Action<EnemyController> OnEnemyDeath;
-        
         [field:SerializeField] public int ScoreAmount { get; private set; }
         
         [SerializeField] private float minShootingInterval;
         [SerializeField] private float maxShootingInterval;
         [SerializeField] private EnemyBullet bulletPrefab;
-        
-        private Attack _enemyAttack;
+        [SerializeField] private float timeBeforeDie;
 
+        private Animator _animator;
+        private Attack _enemyAttack;
+        private static readonly int hit = Animator.StringToHash("hit");
         private void Start()
         {
+            _animator = GetComponent<Animator>();
             _enemyAttack = new AttackWithRandomDelay(minShootingInterval, maxShootingInterval, 
                 bulletPrefab.gameObject, gameObject.transform);
             
@@ -39,7 +41,15 @@ namespace Enemy
         }
         public void Die()
         {
+            DieAsync(CancellationToken.None).Forget();
+        }
+
+        private async UniTask DieAsync(CancellationToken token)
+        {
             OnEnemyDeath?.Invoke(this);
+            
+            _animator.SetTrigger(hit);
+            await UniTask.Delay(TimeSpan.FromSeconds(timeBeforeDie), cancellationToken: token);
             Destroy(gameObject);
         }
         private async UniTask StartAttackLoop(CancellationToken token)
